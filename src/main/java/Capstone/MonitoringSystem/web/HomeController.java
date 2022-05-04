b72a48366a78156368409f16752bcabaa12763f7
eunchannam <namhm23@gmail.com>
2022-05-04 20:45:29 +0900

    edit HomeController by using login

--- src/main/java/Capstone/MonitoringSystem/web/HomeController.java
+++ src/main/java/Capstone/MonitoringSystem/web/HomeController.java
@@ -1,13 +1,19 @@
 package Capstone.MonitoringSystem.web;
 
+import Capstone.MonitoringSystem.LoginConst;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
+    public String home(@SessionAttribute(value = LoginConst.LOGIN_MANAGER, required = false) Long managerId) {
+
+        if (managerId == null) {
+            return "start";
+        }
         return "main";
     }
 }
